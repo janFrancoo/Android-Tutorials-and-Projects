8a090e1118072781f041091fa576831923c86dfd
Eren Sırakaya <45636602+janFrancoo@users.noreply.github.com>
2019-12-17 00:22:30 +0300

    Create UserPost.java

---
+++ InstagramClone/app/java/UserPost.java
@@ -0,0 +1,13 @@
+package com.janfranco.instaclone;
+
+public class UserPost {
+
+    String userEmail, userComment, userImage;
+
+    public UserPost(String userEmail, String userComment, String userImage) {
+        this.userEmail = userEmail;
+        this.userComment = userComment;
+        this.userImage = userImage;
+    }
+
+}
